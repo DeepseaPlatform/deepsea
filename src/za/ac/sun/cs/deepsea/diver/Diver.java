8dadd6746365ee950219ee323a5e6fb7a887a3ed
Jaco Geldenhuys <geld@sun.ac.za>
2017-08-07 10:30:26 +0200

    Update documentation.
    Signed-off-by: Jaco Geldenhuys <geld@sun.ac.za>

--- src/za/ac/sun/cs/deepsea/diver/Diver.java
+++ src/za/ac/sun/cs/deepsea/diver/Diver.java
@@ -16,7 +16,9 @@ import za.ac.sun.cs.deepsea.logging.LogHandler;
 //import za.ac.sun.cs.green.util.Configuration;
 
 /**
- * Driver for dynamic symbolic execution.
+ * Driver for dynamic symbolic execution. It collects all the settings that
+ * apply to a DEEPSEA "session" and when the {@link #start()} method is called,
+ * it conducts one or more "dives" into the target program.
  * 
  * @author Jaco Geldenhuys (geld@sun.ac.za)
  */
@@ -33,34 +35,40 @@ public class Diver {
 	private final Logger log;
 
 	/**
-	 * 
+	 * A counter of how many {@link Dive} instances have been created.
 	 */
 	private int diveCounter;
 
 	/**
-	 * 
+	 * The fully qualified of the target class. This class should contain a Java
+	 * {@code main} method and it is the class that is run during each dive.
 	 */
 	private String target = null;
 
 	/**
-	 * 
+	 * Arguments pass to the target class when it is run during a dive.
 	 */
 	private String args = null;
 
 	/**
-	 * 
+	 * The database of triggers. Each trigger is a method that will switch the
+	 * dive to symbolic mode. The trigger also describes which arguments are
+	 * treated symbolically, and which arguments stay concrete.
 	 */
 	private List<Trigger> triggers = new LinkedList<>();
 
 	/**
-	 * 
+	 * Whether or not the output of the target program should be displayed
+	 * ({@code true}) or suppressed ({@code false}).
 	 */
 	private boolean produceOutput = false;
 
 	/**
-	 * Constructs a {@link Diver} instance.
+	 * Constructs a {@link Diver} instance. Such an instance represents one
+	 * "session" of DEEPSEA.
 	 * 
-	 * @param name the name for this instance
+	 * @param name
+	 *            the name for this instance
 	 */
 	public Diver(final String name) {
 		this.name = name;
@@ -90,33 +98,40 @@ public class Diver {
 	}
 
 	/**
-	 * @return
+	 * Returns the current value of the dive counter and increments it.
+	 * 
+	 * @return the current value of dive counter
 	 */
 	public int getDiveId() {
 		return diveCounter++;
 	}
 
 	/**
-	 * @return
+	 * Returns the target class.
+	 * 
+	 * @return the fully qualified target class
 	 */
 	public String getTarget() {
 		return target;
 	}
 
 	/**
+	 * Sets the target class.
+	 * 
 	 * @param target
+	 *            the fully qualified target class
 	 */
 	public void setTarget(String target) {
 		this.target = target;
 	}
-	
+
 	/**
 	 * @return
 	 */
 	public String getArgs() {
 		return args;
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -130,10 +145,16 @@ public class Diver {
 	public void addTrigger(Trigger trigger) {
 		triggers.add(trigger);
 	}
-	
+
 	/**
+	 * Finds the first trigger that matches the given method and class. If no
+	 * matching trigger is found, the method returns {@code null}.
+	 * 
 	 * @param method
-	 * @return
+	 *            the method to match
+	 * @param className
+	 *            the name of the class to match
+	 * @return the first matching trigger or {@code null}
 	 */
 	public Trigger findTrigger(Method method, String className) {
 		for (Trigger trigger : triggers) {
@@ -143,40 +164,40 @@ public class Diver {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * @return
 	 */
 	public boolean isProducintOutput() {
 		return produceOutput;
 	}
-	
+
 	/**
 	 * @param produceOutput
 	 */
 	public void produceOutput(boolean produceOutput) {
 		this.produceOutput = produceOutput;
 	}
-	
+
 	/**
-	 * Run the diver.  
+	 * Run the diver.
 	 */
 	public void start() {
-//		Green solver = new Green("DEEPSEA");
-//		Properties props = new Properties();
-//		props.setProperty("green.services", "model");
-//		props.setProperty("green.service.model", "(bounder z3java)");
-//		props.setProperty("green.service.model.bounder", "za.ac.sun.cs.green.service.bounder.BounderService");				
-//		props.setProperty("green.service.model.z3java", "za.ac.sun.cs.green.service.z3.ModelZ3JavaService");
-//		// props.setProperty("", "/Users/jaco/Documents/RESEARCH/01/SYMEXE/Z3/build/z3");
-//		Configuration config = new Configuration(solver, props);
-//		config.configure();
+		//		Green solver = new Green("DEEPSEA");
+		//		Properties props = new Properties();
+		//		props.setProperty("green.services", "model");
+		//		props.setProperty("green.service.model", "(bounder z3java)");
+		//		props.setProperty("green.service.model.bounder", "za.ac.sun.cs.green.service.bounder.BounderService");				
+		//		props.setProperty("green.service.model.z3java", "za.ac.sun.cs.green.service.z3.ModelZ3JavaService");
+		//		// props.setProperty("", "/Users/jaco/Documents/RESEARCH/01/SYMEXE/Z3/build/z3");
+		//		Configuration config = new Configuration(solver, props);
+		//		config.configure();
 
 		Dive d = new Dive(this);
 		d.dive();
-//		Instance instance = new Instance(solver, null, d.getPathCondition());
-//		@SuppressWarnings({ "unchecked", "unused" })
-//		Map<IntVariable,Object> model = (Map<IntVariable,Object>) instance.request("model"); 
+		//		Instance instance = new Instance(solver, null, d.getPathCondition());
+		//		@SuppressWarnings({ "unchecked", "unused" })
+		//		Map<IntVariable,Object> model = (Map<IntVariable,Object>) instance.request("model"); 
 		log.info("%%%% " + d.getPathCondition());
 		log.info("Done.");
 	}
