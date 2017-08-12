55274f29d04d05272bffc400df34827d610da6e1
Jaco Geldenhuys <geld@sun.ac.za>
2017-08-12 12:57:41 +0200

    Added counterexample for nonlinear approximation.
    Signed-off-by: Jaco Geldenhuys <geld@sun.ac.za>

---
+++ examples/spf/DART2.java
@@ -0,0 +1,23 @@
+package spf;
+
+public class DART2 {
+
+	public static void test(int x, int y) {
+		if (x*x > 0){
+			if(x>0 && y==10)
+				abort();
+		} else {
+			if (x>0 && y==20)
+				abort();
+		}
+	}
+	
+	public static void abort() {
+		System.out.println("ABORT");
+	}
+	
+	public static void main(String[] args) {
+		test(2,10);
+	}
+	
+}
