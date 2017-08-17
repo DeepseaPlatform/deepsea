d7b561cd815065d01cedb6dbbb4b1cc1282d9c1b
Jaco Geldenhuys <geld@sun.ac.za>
2017-08-17 22:44:28 +0200

    Add ACONST_NULL instruction
    Working towards #12
    
    Signed-off-by: Jaco Geldenhuys <geld@sun.ac.za>

--- src/za/ac/sun/cs/deepsea/instructions/ACONST_NULL.java
+++ src/za/ac/sun/cs/deepsea/instructions/ACONST_NULL.java
@@ -1,6 +1,12 @@
 package za.ac.sun.cs.deepsea.instructions;
 
+import com.sun.jdi.Location;
+import com.sun.jdi.event.StepEvent;
+
 import za.ac.sun.cs.deepsea.diver.Stepper;
+import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
+import za.ac.sun.cs.deepsea.diver.Symbolizer;
+import za.ac.sun.cs.green.expr.Operation;
 
 public class ACONST_NULL extends Instruction {
 
@@ -8,4 +14,15 @@ public class ACONST_NULL extends Instruction {
 		super(stepper, position, 1);
 	}
 	
+	@Override
+	public void execute(StepEvent event, Location loc, Symbolizer symbolizer) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		frame.push(Operation.ZERO);
+	}
+	
+	@Override
+	public String toString() {
+		return "aconst_null";
+	}
+
 }
