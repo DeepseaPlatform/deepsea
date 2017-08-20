281345a2030691708625334ec28aecd27ef1fd9b
Jaco Geldenhuys <geld@sun.ac.za>
2017-08-20 18:33:17 +0200

    Add documentation for Reporter
    Working towards #1.
    
    Signed-off-by: Jaco Geldenhuys <geld@sun.ac.za>

--- src/za/ac/sun/cs/deepsea/Reporter.java
+++ src/za/ac/sun/cs/deepsea/Reporter.java
@@ -1,11 +1,29 @@
-package za.ac.sun.cs.deepsea.explorer;
+package za.ac.sun.cs.deepsea;
 
 import java.io.PrintWriter;
 
+/**
+ * Contract that is followed by all component that writes a report at the end of
+ * a DEEPSEA run.
+ * 
+ * @author Jaco Geldenhuys (geld@sun.ac.za)
+ */
 public interface Reporter {
 
+	/**
+	 * Returns a name for this reporter.
+	 * 
+	 * @return the name of the reporter
+	 */
 	public String getName();
-	
+
+	/**
+	 * Writes a report that summarizes the execution of a component at the end
+	 * of a DEEPSEA run.
+	 * 
+	 * @param out
+	 *            the destination to which the report must be written
+	 */
 	public void report(PrintWriter out);
 
 }
