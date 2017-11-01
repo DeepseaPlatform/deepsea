90f23a40981f0f608f2f5eb39e8ea3098b8eda5a
Jaco Geldenhuys <geld@sun.ac.za>
2017-11-02 01:00:10 +0200

    Add examples of how delegates are used
    For the moment, the Math delegate does not add its additional guard to
    the path condition.
    
    Firstly, GREEN needs an update to handle such guards which may contain
    complex expressions.  This involves not simplifying the expression and
    passing it to Z3 untouched.  In the long run, GREEN should also simplify
    complex expressions, but it has never been used in this new context and
    it does not currently implement this functionality.
    
    Secondly, DEEPSEA needs to be adjusted to handle the conjunction of
    additional guards separately and pass it to the Explorer as an
    additional path condition.  The Explorer should pass the conjunction of
    the regular old path condition and the additional path condition to the
    solver (GREEN), and then it should negate only the regular old path
    condition's conjuncts.  This is not a big change and the code is already
    in place, but it needs to be "activated".

--- src/main/java/za/ac/sun/cs/deepsea/models/Math.java
+++ src/main/java/za/ac/sun/cs/deepsea/models/Math.java
@@ -1,10 +1,32 @@
 package za.ac.sun.cs.deepsea.models;
 
 import za.ac.sun.cs.deepsea.diver.Diver;
+import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
+import za.ac.sun.cs.deepsea.diver.Symbolizer;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Operation.Operator;
 
 public class Math {
 
 	public Math(Diver diver) {
+	}
+
+	public boolean max_II_I(Symbolizer symbolizer) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		Expression arg0 = frame.pop();
+		Expression arg1 = frame.pop();
+		Expression var = new IntVariable(Symbolizer.getNewVariableName(), -1000, 1000);
+		Expression pc = new Operation(Operator.OR,
+				new Operation(Operator.AND,
+						new Operation(Operator.GE, arg0, arg1),
+						new Operation(Operator.EQ, arg0, var)),
+				new Operation(Operator.AND,
+						new Operation(Operator.LT, arg0, arg1),
+						new Operation(Operator.EQ, arg1, var)));
+		frame.push(var);
+		return true;
 	}
 
 }
