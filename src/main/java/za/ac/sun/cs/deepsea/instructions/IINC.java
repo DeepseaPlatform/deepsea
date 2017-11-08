0024e6648ea1ac7bbcd53a2411be04391033907e
Jaco Geldenhuys <geld@sun.ac.za>
2017-11-08 22:05:25 +0200

    Add implementation of IINC instruction.

--- src/main/java/za/ac/sun/cs/deepsea/instructions/IINC.java
+++ src/main/java/za/ac/sun/cs/deepsea/instructions/IINC.java
@@ -1,6 +1,14 @@
 package za.ac.sun.cs.deepsea.instructions;
 
+import com.sun.jdi.event.StepEvent;
+
 import za.ac.sun.cs.deepsea.diver.Stepper;
+import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
+import za.ac.sun.cs.deepsea.diver.Symbolizer;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Operation.Operator;
 
 public class IINC extends Instruction {
 
@@ -25,6 +33,21 @@ public class IINC extends Instruction {
 	@Override
 	public int getSize() {
 		return 3;
+	}
+
+	@Override
+	public void execute(StepEvent event, Symbolizer symbolizer) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		Expression e1 = new IntConstant(value);
+		Expression e0 = frame.getLocal(index);
+		frame.push(new Operation(Operator.ADD, e0, e1));
+	}
+	
+	@Override
+	public String toString() {
+		sb.setLength(0);
+		sb.append("iinc #").append(index).append(' ').append(value);
+		return sb.toString();
 	}
 
 }
