546f9a4b99ff52a1e8f2b344232fb778945cdb22
Willem Visser <visserw@Willems-MacBook-Air-2.local>
2017-11-10 10:13:05 -0800

    examples with some issues DSEBetter.*

--- src/main/java/za/ac/sun/cs/deepsea/instructions/IFGT.java
+++ src/main/java/za/ac/sun/cs/deepsea/instructions/IFGT.java
@@ -1,6 +1,14 @@
 package za.ac.sun.cs.deepsea.instructions;
 
+import com.sun.jdi.event.StepEvent;
+
 import za.ac.sun.cs.deepsea.diver.Stepper;
+import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
+import za.ac.sun.cs.deepsea.diver.Symbolizer;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Operation.Operator;
 
 public class IFGT extends Instruction {
 
@@ -19,5 +27,21 @@ public class IFGT extends Instruction {
 	public int getSize() {
 		return 3;
 	}
+	
+	@Override
+	public void execute(StepEvent event, Symbolizer symbolizer) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		Expression e0 = new IntConstant(0);
+		Expression e1 = frame.pop();
+		symbolizer.pushConjunct(new Operation(Operator.GT, e1, e0), position + offset);
+	}
+	
+	@Override
+	public String toString() {
+		sb.setLength(0);
+		sb.append("ifgt -> ").append(position + offset);
+		return sb.toString();
+	}
+
 
 }
