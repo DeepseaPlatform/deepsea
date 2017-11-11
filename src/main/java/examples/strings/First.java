cfb2f16f88db8daaa94fd9cc3247c15a2ac58bc5
Jaco Geldenhuys <geld@sun.ac.za>
2017-11-11 04:51:05 +0200

    Add String.endsWith delegate, enhance example.

--- src/main/java/examples/strings/First.java
+++ src/main/java/examples/strings/First.java
@@ -4,17 +4,22 @@ public class First {
 
 	public static int test(String str) {
 		if (str.startsWith("bl")) {
-			System.out.println("Match");
-			return 1;
+			if (str.endsWith("ue")) {
+				System.out.println("Match");
+				return 0;
+			} else {
+				System.out.println("No match (A)");
+				return 1;
+			}
 		}
 		else {
-			System.out.println("No match");
-			return 0;
+			System.out.println("No match (B)");
+			return 2;
 		}
 	}
 	
 	public static void main(String[] args) {
-		test("hel");
+		test("hell");
 	}
 
 }
--- src/main/java/za/ac/sun/cs/deepsea/models/String.java
+++ src/main/java/za/ac/sun/cs/deepsea/models/String.java
@@ -55,6 +55,45 @@ public class String {
 		return true;
 	}
 
+	public boolean endsWith__Ljava_1lang_1String_2__Z(Symbolizer symbolizer, ThreadReference thread) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		int prefixAddress = intConstantValue(frame.pop());
+		int thisAddress = intConstantValue(frame.pop());
+		int prefixLength = intConstantValue(symbolizer.getField(prefixAddress, "length"));
+		int thisLength = intConstantValue(symbolizer.getField(thisAddress, "length"));
+		if (thisLength >= prefixLength) {
+			Expression guard = null;
+			for (int i = 0; i < prefixLength; i++) {
+				Expression prefixChar = symbolizer.getField(prefixAddress, "" + i);
+				Expression thisChar = symbolizer.getField(thisAddress, "" + (i + thisLength - prefixLength));
+				Expression eq = Operation.apply(Operator.EQ, prefixChar, thisChar);
+				if (i == 0) {
+					guard = eq;
+				} else {
+					guard = Operation.apply(Operator.AND, guard, eq);
+				}
+			}
+			if (guard == null) {
+				frame.push(Operation.ONE); // |prefix| == 0, so result is always TRUE (=1)
+			} else {
+				Expression var = new IntVariable(Symbolizer.getNewVariableName(), 0, 1);
+				Expression pc = Operation.apply(Operator.OR,
+						Operation.apply(Operator.AND,
+								guard,
+								Operation.apply(Operator.EQ, var, Operation.ONE)),
+						Operation.apply(Operator.AND,
+								Operation.apply(Operator.NOT, guard),
+								Operation.apply(Operator.EQ, var, Operation.ZERO)));
+				symbolizer.pushExtraConjunct(pc);
+				frame.push(var);
+			}
+		} else {
+			// |target array| < |pattern array|, so it can never match
+			frame.push(Operation.ZERO);
+		}
+		return true;
+	}
+	
 	private int intConstantValue(Expression expr) {
 		assert expr instanceof IntConstant;
 		return ((IntConstant) expr).getValue();
