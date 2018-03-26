1febd9881f21b7f1f56b6342e5b86b53e72da587
Jaco Geldenhuys <geld@sun.ac.za>
2018-03-26 04:39:56 +0200

    Add object transmitting tasks/results

---
+++ src/main/java/za/ac/sun/cs/deepsea/distributed/TaskResult.java
@@ -0,0 +1,56 @@
+package za.ac.sun.cs.deepsea.distributed;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.util.Strings;
+
+import za.ac.sun.cs.green.expr.Constant;
+
+public class TaskResult implements Serializable {
+
+	private static final long serialVersionUID = -1792007788307029197L;
+
+	private final String path;
+	
+	private final Map<String, Constant> values;
+
+	public static final TaskResult EMPTY = new TaskResult();
+
+	private TaskResult() {
+		path = Strings.EMPTY;
+		values = Collections.emptyMap();
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public Map<String, Constant> getValues() {
+		return Collections.unmodifiableMap(values);
+	}
+
+	public String intoString() throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		oos.writeObject(this);
+		oos.close();
+		return Base64.getEncoder().encodeToString(baos.toByteArray()); 
+	}
+	
+	public static TaskResult fromString(String s) throws IOException, ClassNotFoundException {
+		byte [] data = Base64.getDecoder().decode( s );
+		ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+		Object o = ois.readObject();
+		ois.close();
+		return (TaskResult) o;
+	}
+
+}
