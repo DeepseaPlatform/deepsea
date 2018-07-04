cb6fb08dd43906643087dc72b85aeb384e12d27a
Jaco Geldenhuys <geld@sun.ac.za>
2018-07-04 22:38:51 +0200

    Add IFGE and IFLT instructions

--- src/main/java/za/ac/sun/cs/deepsea/instructions/IFGE.java
+++ src/main/java/za/ac/sun/cs/deepsea/instructions/IFGE.java
@@ -1,6 +1,14 @@
 package za.ac.sun.cs.deepsea.instructions;
 
+import com.sun.jdi.event.StepEvent;
+
 import za.ac.sun.cs.deepsea.diver.Stepper;
+import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
+import za.ac.sun.cs.deepsea.diver.Symbolizer;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Operation.Operator;
 
 public class IFGE extends Instruction {
 
@@ -18,6 +26,21 @@ public class IFGE extends Instruction {
 	@Override
 	public int getSize() {
 		return 3;
+	}
+
+	@Override
+	public void execute(StepEvent event, Symbolizer symbolizer) {
+		SymbolicFrame frame = symbolizer.getTopFrame();
+		Expression e0 = new IntConstant(0);
+		Expression e1 = frame.pop();
+		symbolizer.pushConjunct(new Operation(Operator.GE, e1, e0), position + offset);
+	}
+	
+	@Override
+	public String toString() {
+		sb.setLength(0);
+		sb.append("iflt -> ").append(position + offset);
+		return sb.toString();
 	}
 
 }
