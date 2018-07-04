703038016d81a51a24a4313f47373eca41257f23
Jaco Geldenhuys <geld@sun.ac.za>
2018-07-04 22:39:33 +0200

    Improve IADD and ISUB instructions
    Constant operands are now folded into new constants.

--- src/main/java/za/ac/sun/cs/deepsea/instructions/ISUB.java
+++ src/main/java/za/ac/sun/cs/deepsea/instructions/ISUB.java
@@ -6,6 +6,7 @@ import za.ac.sun.cs.deepsea.diver.Stepper;
 import za.ac.sun.cs.deepsea.diver.SymbolicFrame;
 import za.ac.sun.cs.deepsea.diver.Symbolizer;
 import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.IntConstant;
 import za.ac.sun.cs.green.expr.Operation;
 import za.ac.sun.cs.green.expr.Operation.Operator;
 
@@ -20,7 +21,11 @@ public class ISUB extends Instruction {
 		SymbolicFrame frame = symbolizer.getTopFrame();
 		Expression e1 = frame.pop();
 		Expression e0 = frame.pop();
-		frame.push(new Operation(Operator.SUB, e0, e1));
+		if ((e0 instanceof IntConstant) && (e1 instanceof IntConstant)) {
+			frame.push(new IntConstant(((IntConstant) e0).getValue() - ((IntConstant) e1).getValue()));
+		} else {
+			frame.push(new Operation(Operator.SUB, e0, e1));
+		}
 	}
 	
 	@Override
